5af704588cb9531c36f40129ca654b687381b0a5
Jared Nalwalker <jared.nalwalker@gmail.com>
2017-01-31 21:06:03 -0700

    added json

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 var http = require('http');
 var fs = require('fs');
+var EventEmitter = require('events').EventEmitter;
+
 var port = 8080;
-http.createServer(function functionName(request, response) {
+var logger = new EventEmitter();
+
+logger.on('error', function(message){
+  console.log('ERROR: ' + message);
+})
+
+http.createServer(function(request, response) {
   fs.readFile('index.html', function(err, content) {
       if(err) {
         response.writeHead(404, {'Content-Type': 'text/html'});
+        logger.emit('error', err);
         response.end('Failed')
       } else {
         response.writeHead(200, {'Content-Type': 'text/html'});
